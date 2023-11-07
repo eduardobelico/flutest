1fac902df33d7b5672c59ecf9b4222269efb03fb
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-11-07 11:29:05 -0300

    Text, Button e Overflow.

--- lib/main.dart
+++ lib/main.dart
@@ -15,33 +15,78 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(color: Colors.white, width: 200, height: 200,),
-              Container(color: Colors.cyan,width: 100, height: 100,),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(color: Colors.amber, width: 200, height: 200,),
-              Container(color: Colors.teal,width: 100, height: 100,),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(color: Colors.greenAccent, width: 50, height: 50,),
-              Container(color: Colors.deepOrangeAccent, width: 50, height: 50,),
-              Container(color: Colors.pinkAccent, width: 50, height: 50,),
-            ],)
-        ],
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.black,
+                  width: 200,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.cyan,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.amber,
+                  width: 200,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.teal,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.greenAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.deepOrangeAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.orange,
+              width: 300,
+              height: 30,
+              child: Text(
+                'Diamante Amarelo',
+                style: TextStyle(color: Colors.black, fontSize: 28),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(onPressed: () {
+              print('Botão apertado!');
+            }, child: Text('Aperte o botão!'))
+          ],
+        ),
       ),
     );
   }
@@ -73,4 +118,3 @@ class MyApp extends StatelessWidget {
     ],
     )
  */
-
